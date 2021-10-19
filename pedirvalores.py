50926c42981e2624f34fab21307feaec6ae8b04f
AdriHappies <adrifegar97@gmail.com>
2021-10-19 17:13:55 +0200

    commit

---
+++ pedirvalores.py
@@ -0,0 +1,7 @@
+#Pedir valores a usuarios
+print("Escriba un texto:")
+variable = input()
+print("Escriba un número:")
+variable2 = int(input())
+variable2 = variable2 * variable2
+print(variable + str(variable2))
