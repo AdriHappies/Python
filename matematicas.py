b3dd9676e19d8c2d5f64c000b7a452a0c33a4e44
AdriHappies <adrifegar97@gmail.com>
2021-10-19 17:04:03 +0200

    commit

---
+++ matematicas.py
@@ -0,0 +1,16 @@
+print("Ejemplo de matemáticas")
+
+num1 = 3
+num2 = 22
+suma = num1 + num2
+print("suma = " + str(suma))
+multiplicacion = num1 * num2
+print("multiplicación = " + str(multiplicacion))
+resta = num2 - num1
+print("resta =", resta)
+division = num2 / num1
+print("division = " + str(division))
+cociente = num2 // num1
+print("cociente = " + str(cociente))
+resto = num2 % num1
+print("resto = " + str(resto))
--- prueba.py
+++ prueba.py
@@ -1,1 +1,8 @@
 print("Primer Paython")
+#No hay que declarar el tipo de variable, solo igualar la variable a un valor
+numero = 198
+texto = "Soy un texto"
+verdadero = False
+
+for i in [numero, texto, verdadero]:
+    print(i)
