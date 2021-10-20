8e372da262c137b6f47f65398b981d6bf107bcb8
AdriHappies <adrifegar97@gmail.com>
2021-10-20 19:56:02 +0200

    commit

--- validarmail.py
+++ validarmail.py
@@ -10,8 +10,8 @@ elif (mail[0] == "@") or (mail[0] == ".") or (mail[len(mail)-1] == "@") or (mail
     print("Error, el mail no puede empezar o acabar con @ o .")
 elif (mail.find("@") != mail.rfind("@")):
     print("Error, el mail no puede contener más de una @")
-elif mail.find("@") > mail.find("."):
-    print("Error, no puede haber un punto antes de la @")
+elif mail.find("@") > mail.rfind("."):
+    print("Error, no hay un punto tras la @ para marcar el dominio")
 elif (len(mail[mail.find(".") + 1:]) < 2) or (len(mail[mail.find(".") + 1:]) > 4) :
     print("Error, dominio de longitud incorrecta")
 else:
