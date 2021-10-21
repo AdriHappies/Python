b44dd1962402efc8e04ffbd5dd9db3dbcf8062ad
AdriHappies <adrifegar97@gmail.com>
2021-10-21 18:03:52 +0200

    commit

--- ejemplometodos.py
+++ ejemplometodos.py
@@ -1,12 +1,17 @@
 def saludo(nombre):
     print("\nBienvenido, " + nombre)
 
-def despedida(nombre, dia):
-    print("\nHa sido un placer hoy " + dia + ", " + nombre)
+def diasemana(nombre, dia):
+    print("\nAsí que hoy es " + dia + ", gracias!")
+    despedida(nombre)
 
+def despedida(nombre):
+    print("\nHasta luego " + nombre)
+
+#main
 print("\n¿Cuál es tu nombre?")
 name = input()
 saludo(name)
 print("\n¿Qué día de la semana es hoy?")
 day = input()
-despedida(name, day)
+diasemana(name, day)
