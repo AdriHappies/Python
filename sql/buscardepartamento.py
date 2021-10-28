5cad382a55090e4f4ef19729de54c67d28dd5ffc
AdriHappies <adrifegar97@gmail.com>
2021-10-28 20:26:09 +0200

    commit

---
+++ sql/buscardepartamento.py
@@ -0,0 +1,27 @@
+import pyodbc
+
+print("Buscador de departamentos")
+
+servidor = "LOCALHOST\SQLEXPRESS"
+bbdd = "HOSPITAL"
+usuario = "sa"
+password = "azure"
+cadenaconexion = ("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=" +
+servidor + "; DATABASE=" + bbdd + "; UID=" + usuario + "; PWD=" + password)
+
+conexion = pyodbc.connect(cadenaconexion)
+
+#vamos a pedir el numero de departamento
+print("Departamento a buscar")
+numero = input()
+
+sql = "select dept_no, dnombre, loc from dept where dept_no =" + numero
+cursor = conexion.execute(sql)
+row = cursor.fetchone()
+if row == "None":
+    print("No existe el departamento")
+else:
+    print(row.dnombre + ", " + row.loc)
+
+cursor.close()
+conexion.close()
--- sql/primeraconsultasql.py
+++ sql/primeraconsultasql.py
@@ -7,11 +7,33 @@ password = "azure"
 cadenaconexion = ("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=" +
 servidor + "; DATABASE=" + bbdd + "; UID=" + usuario + "; PWD=" + password)
 
-try:
-    print("Intentando conectar")
-    conexion = pyodbc.connect(cadenaconexion)
-    print("Conectado")
-except:
-    print("Buff, esto no va bien")
-finally:
-    conexion.close()
+
+print("Intentando conectar")
+conexion = pyodbc.connect(cadenaconexion)
+print("Conectado")
+
+#Cursor se crea con una coneion abierta
+cursor = conexion.cursor()
+#necesitamos una consulta, el cursor maneja consultas select o de accion
+#creamos una consulta select
+sql = "select * from dept"
+#el cursor ejecuta la consulta
+cursor.execute(sql)
+#podemos recuperar una fila
+#row = cursor.fetchone()
+#dibujamos la fila
+#print(row)
+#repetimos fetchone
+#row = cursor.fetchone()
+#print(row)
+#cada vez que se ejecuta el metodo fetch el cursor se mueve una fila
+#no podemos volver a la fila anterior, a no ser que hagamos el execute otra vez
+for row in cursor:
+    #print(row[0])
+    print(row.LOC)
+
+
+
+cursor.close()    
+conexion.close()
+print("----------------\nfin de programa\n-----------------")
